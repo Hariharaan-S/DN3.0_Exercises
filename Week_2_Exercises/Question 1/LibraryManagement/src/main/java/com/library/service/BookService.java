77336f46f72e1cd443fe363e74d67907137c9098
Hariharaan-S <138239947+Hariharaan-S@users.noreply.github.com>
2024-08-06 16:55:26 +0530

    Complete till 2nd task

---
+++ Week_2_Exercises/Question 1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author shari
+ */
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+    
+}
---
+++ Week_2_Exercises/Question 1/LibraryManagement/src/main/java/com/librarymanagement/LibraryManagement/LibraryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.librarymanagement.LibraryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Configuration Successful!");
+        SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+
+}
